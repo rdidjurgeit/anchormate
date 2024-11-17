04e74c96afa4650baa8d902d54a7c664c15e3ad1
rdidjurgeit <raphael0rd@gmail.com>
2024-11-17 11:58:35 +0100

    Seabed use default Unknown

--- anchorages/models.py
+++ anchorages/models.py
@@ -8,7 +8,7 @@ class Anchorage(models.Model):
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     depth = models.FloatField()
-    seabed_type = models.CharField(max_length=50)
+    seabed_type = models.CharField(max_length=50, default="Unknown")
     description = models.TextField()
     added_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
